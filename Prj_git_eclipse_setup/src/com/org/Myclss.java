4f6a5ace58a4982e31a229b1a69fd295bb461f30
sunilmanghrani <sunilmanghrani28@gmail.com>
2021-06-25 18:14:01 +0100

    First commit of my Prj

---
+++ Prj_git_eclipse_setup/src/com/org/Myclss.java
@@ -0,0 +1,31 @@
+package com.org;
+
+public class Myclss {
+
+	public static void main(String[] args) {
+
+		int a = 4;
+		int b = 2;
+
+		int c, e;
+
+		// ********** block 1 start *******************
+		int d, f;
+
+		c = a + b;
+		System.out.println("Sum of a & b : " + c);
+
+		d = a - b;
+		System.out.println("diff of a & b : " + d);
+
+		// ********** block 1 end **********************
+
+		e = a * b;
+		System.out.println("multiple of a & b : " + e);
+
+		f = a % b;
+		System.out.println("division of a & b : " + f);
+
+	}
+
+}
